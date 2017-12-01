f86681dd88c9fe1b1bb1c40dccf6a12b67916ae7
Nicholas Echevarria <nick.echev@gmail.com>
2017-12-01 01:36:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,8 +20,7 @@ function accessElementInArray(array, index) {
     return array[2]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array, element) {
-	   array.shift(element)
-     return array
+	  return array.shift(element) 
 }
 function removeElementFromBeginningOfArray(array) {
     return array.slice(1)
